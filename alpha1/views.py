98803045cfb45f899723b7fe7b7c7246c32bf3d1
marcleord <marcleord@gmail.com>
2024-06-26 18:05:13 +0000

    filtre des data du fichier en fonction d'une query param

--- alpha1/views.py
+++ alpha1/views.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from typing import Optional
 
 def index(request, route: str):
     # return HttpResponse("<h1>path parameter dynamique</h1>")
@@ -12,19 +13,41 @@ def users(request):
     # retourner le contenu du fichier
     chemin = "file.txt"
     separateur = "   @@@   "
-    # print(request.GET)
+    
+    selecteur: Optional[str] = request.GET.get('selecteur')
+    print("selecteur " , selecteur, type(selecteur))
 
     # Vérifier si le fichier existe et lire son contenu
     if os.path.exists(chemin):
         with open(chemin, 'r') as fp:
-            lignes = fp.readlines()
 
-            id_users = []
-            for ligne in lignes:
-                infos = ligne.split(separateur)
-                id_user = infos[0]
-                id_users.append(id_user)
-        contenu_fichier = "\n".join(id_users)
+            # S'il n'y a pas de sélecteur
+            if selecteur == None:
+                # On récupère tout le contenu du fichier
+                contenu_fichier = fp.read()
+            else:
+                # Sinon on va filtrer en fonction du sélecteur
+                content_list = []
+                lignes = fp.readlines()
+                for ligne in lignes:
+                    infos = ligne.split(separateur)
+                    id_user = infos[0]
+                    heure_consult = infos[1]
+                    id_device = infos[2]
+
+                    chaine = ""
+                    
+                    if 'id_user' in selecteur:
+                        chaine += id_user + " "
+                    if 'heure' in selecteur:
+                        chaine += heure_consult + " "
+                    if 'id_device' in selecteur:
+                        chaine += id_device
+
+                    content_list.append(chaine)
+                # Qu'importe le nombre de filtres sélectionnés, ils sont tous cassé 
+                # et insérés dans la chaine <contenu_fichier>
+                contenu_fichier = "\n".join(content_list)
 
     else:
         contenu_fichier = "Le fichier de file.txt est vide ou n'existe pas."
